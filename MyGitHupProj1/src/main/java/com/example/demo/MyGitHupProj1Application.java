e8448f56daa9dcce0b7e4cae7282617870593672
karunakaran.v <karunakaran.v@INCN-IT-LP062.uaeexchange.org>
2020-11-27 18:05:31 +0530

    First Commit

--- MyGitHupProj1/src/main/java/com/example/demo/MyGitHupProj1Application.java
+++ MyGitHupProj1/src/main/java/com/example/demo/MyGitHupProj1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTestApplication {
+public class MyGitHupProj1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTestApplication.class, args);
+		SpringApplication.run(MyGitHupProj1Application.class, args);
 	}
 
 }
